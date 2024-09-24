8ad147154f9350ed29ffec3ce4a9e855143337ce
Nardi <lordnardin2003@mail.com>
2024-09-24 19:51:19 +0200

    Added some plugins (fugitive, harpoon, lsp, undo tree and it's relative remaps)

--- lua/nardi/packer.lua
+++ lua/nardi/packer.lua
@@ -4,29 +4,38 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-  use ({ "catppuccin/nvim", as = "catppuccin", 
-  	config = function()
+	use ({ "catppuccin/nvim", as = "catppuccin", 
+	config = function()
 		vim.cmd('colorscheme catppuccin-mocha')
 	end})
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-    
-    use {'nvim-treesitter/playground'}
-	
-    use {'ThePrimeagen/harpoon'}
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
 
-    use {'github/copilot.vim'}
+	use {'nvim-treesitter/playground'}
+
+	use {'ThePrimeagen/harpoon'}
+
+	use {'github/copilot.vim'}
+
+	use {'mbbill/undotree'}
+
+	use {'tpope/vim-fugitive'}
+
+	use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
+	use({'neovim/nvim-lspconfig'})
+	use({'hrsh7th/nvim-cmp'})
+	use({'hrsh7th/cmp-nvim-lsp'})
 
   end)
